c6bea7ff6dc1e9f0ae3bb77dda54a1a72bb7f734
One Mark <lakshsolanki848@gmail.com>
2025-07-20 10:25:31 +0530

    "layout_update"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <Navbar />
-        
+        <Navbar /> 
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
